f51e76d4c251184498c1bf71d913c881bd7e05df
ankush-amura <ankush@amuratech.com>
2018-11-18 23:39:00 +0530

    devise-user_setup complete

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :confirmable,:registerable,
+         :recoverable, :rememberable,:trackable, :validatable
+
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+
+  field :confirmation_token,   type: String
+  field :confirmed_at,         type: Time
+  field :confirmation_sent_at, type: Time
+  field :unconfirmed_email,    type: String # Only if using reconfirmable
+
+  field :name, type: String, default: ""
+  field :role, type: String, default: "Developer"
+
+  def self.available_roles
+    return ["Manager", "Developer"]
+  end
+end
